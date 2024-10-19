3644019712955a82f71b137b3e48b6cba35c48c8
Faizan Habib <faizan9cm@gmail.com>
2024-10-19 17:28:41 +0530

    Modified FaceDetectionModule.py

--- FaceDetection/FaceDetectionModule.py
+++ FaceDetection/FaceDetectionModule.py
@@ -27,15 +27,36 @@ class FaceDetector():
 
                 bboxs.append([id, bbox, detection.score])
 
-                cv2.rectangle(img, bbox, (255, 0, 255), 2)
-                cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+                if draw:
+                    img = self.fancyDraw(img, bbox)
+
+                    # cv2.rectangle(img, bbox, (255, 0, 255), 2)
+                    cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
 
         return img, bboxs
     
+    def fancyDraw(self, img, bbox, l=20, t=3, rt=1):
+        x, y, w, h = bbox
+        x1, y1 = x + w, y + h
+        cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        
+        # Top left x, y
+        cv2.line(img, (x, y), (x + l, y), (255, 0, 255), t)
+        cv2.line(img, (x, y), (x, y + l), (255, 0, 255), t)
+        
+        # Top right x1, y
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1, y + l), (255, 0, 255), t)
 
+        # Bottom left x, y1
+        cv2.line(img, (x, y1), (x, y1 - l), (255, 0, 255), t)
+        cv2.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
+        
+        # Bottom right x1, y1
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
+        return img
     
-
-
 
 
 def main():
@@ -49,10 +70,10 @@ def main():
         print(bboxs)
 
         cTime = time.time()
-        fps = 1 / (pTime - cTime)
+        fps = 1 / (cTime - pTime)
         pTime = cTime
 
-        cv2.putText(img, f'FPS: {int(fps)}', (70, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 25), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
